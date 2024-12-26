6562bca863e116ddd0cbdff6fe16af227cb53869
RobinRMC <github@robinmail.nl>
2024-12-26 01:14:34 +0100

    Add BetterMicrophone, BetterScreenshare & PhilsPluginLibrary

--- src/plusplugins/betterMicrophone.desktop/index.tsx
+++ src/plusplugins/betterMicrophone.desktop/index.tsx
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { PluginAuthor, PluginDef } from "@utils/types";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
 
 import { addSettingsPanelButton, Emitter, MicrophoneSettingsIcon, removeSettingsPanelButton } from "../philsPluginLibrary";
 import { PluginInfo } from "./constants";
@@ -12,29 +13,18 @@ import { openMicrophoneSettingsModal } from "./modals";
 import { MicrophonePatcher } from "./patchers";
 import { initMicrophoneStore } from "./stores";
 
-export default new class Plugin implements PluginDef {
-    readonly name: string;
-    readonly description: string;
-    readonly authors: PluginAuthor[];
-    readonly dependencies: string[];
-
-    public microphonePatcher?: MicrophonePatcher;
-
-    constructor() {
-        this.name = PluginInfo.PLUGIN_NAME;
-        this.description = PluginInfo.DESCRIPTION;
-        this.authors = [PluginInfo.AUTHOR, ...Object.values(PluginInfo.CONTRIBUTORS)] as PluginAuthor[];
-        this.dependencies = ["PhilsPluginLibrary"];
-    }
-
+export default definePlugin({
+    name: "BetterMicrophone",
+    description: "This plugin allows you to further customize your microphone",
+    authors: [Devs.philhk],
+    dependencies: ["PhilsPluginLibrary"],
     start(): void {
         initMicrophoneStore();
 
         this.microphonePatcher = new MicrophonePatcher().patch();
 
         addSettingsPanelButton({ name: PluginInfo.PLUGIN_NAME, icon: MicrophoneSettingsIcon, tooltipText: "Microphone Settings", onClick: openMicrophoneSettingsModal });
-    }
-
+    },
     stop(): void {
         this.microphonePatcher?.unpatch();
 
@@ -42,4 +32,4 @@ export default new class Plugin implements PluginDef {
 
         removeSettingsPanelButton(PluginInfo.PLUGIN_NAME);
     }
-};
+});
---
+++ src/plusplugins/betterScreenshare.desktop/index.tsx
@@ -0,0 +1,60 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+import { addSettingsPanelButton, Emitter, removeSettingsPanelButton, ScreenshareSettingsIcon } from "../philsPluginLibrary";
+import { PluginInfo } from "./constants";
+import { openScreenshareModal } from "./modals";
+import { ScreenshareAudioPatcher, ScreensharePatcher } from "./patchers";
+import { replacedScreenshareModalComponent } from "./patches";
+import { initScreenshareAudioStore, initScreenshareStore } from "./stores";
+
+export default definePlugin({
+    name: "BetterScreenshare",
+    description: "This plugin allows you to further customize your screen sharing.",
+    authors: [Devs.philhk],
+    dependencies: ["PhilsPluginLibrary"],
+    patches: [
+        {
+            find: "Messages.SCREENSHARE_RELAUNCH",
+            replacement: {
+                match: /(function .{1,2}\(.{1,2}\){)(.{1,40}(?=selectGuild).+?(?:]}\)}\)))(})/,
+                replace: "$1return $self.replacedScreenshareModalComponent(function(){$2}, this, arguments)$3"
+            }
+        }
+    ],
+    settings: definePluginSettings({
+        hideDefaultSettings: {
+            type: OptionType.BOOLEAN,
+            description: "Hide Discord screen sharing settings",
+            default: true,
+        }
+    }),
+    start(): void {
+        initScreenshareStore();
+        initScreenshareAudioStore();
+        this.screensharePatcher = new ScreensharePatcher().patch();
+        this.screenshareAudioPatcher = new ScreenshareAudioPatcher().patch();
+
+        addSettingsPanelButton({
+            name: PluginInfo.PLUGIN_NAME,
+            icon: ScreenshareSettingsIcon,
+            tooltipText: "Screenshare Settings",
+            onClick: openScreenshareModal
+        });
+    },
+    stop(): void {
+        this.screensharePatcher?.unpatch();
+        this.screenshareAudioPatcher?.unpatch();
+        Emitter.removeAllListeners(PluginInfo.PLUGIN_NAME);
+
+        removeSettingsPanelButton(PluginInfo.PLUGIN_NAME);
+    },
+    replacedScreenshareModalComponent
+});
