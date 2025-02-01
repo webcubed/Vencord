f1c3d92c825d3901277fe9d94ca3b248d5766a21
RobinRMC <github@robinmail.nl>
2025-02-01 23:53:20 +0100

    Update ReplaceActivityTypes

--- src/plusplugins/replaceActivityTypes/ReplaceSettings.tsx
+++ src/plusplugins/replaceActivityTypes/ReplaceSettings.tsx
@@ -1,34 +1,20 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 your mom lol
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { CheckedTextInput } from "@components/CheckedTextInput";
 import { Margins } from "@utils/margins";
 import { identity } from "@utils/misc";
-import { findByCodeLazy } from "@webpack";
-import { Card, Forms, React, Select, SnowflakeUtils, Switch } from "@webpack/common";
+import { Card, Forms, PresenceStore, React, Select, SnowflakeUtils, Switch, TextInput, UserStore } from "@webpack/common";
 
-import { makeEmptyAppId } from ".";
-import { ActivityType, RpcApp, SettingsProps } from "./types";
+import { Activity, ActivityType, AppIdSetting, makeEmptyAppId } from ".";
 
-
-const fetchApplicationsRPC = findByCodeLazy("APPLICATION_RPC(", "Client ID");
-
-const cachedApps: any = {};
-async function lookupApp(appId: string): Promise<RpcApp | null> {
-    if (cachedApps[appId]) return cachedApps[appId];
-    const socket: any = {};
-    try {
-        await fetchApplicationsRPC(socket, appId);
-        console.log(`Lookup finished for ${socket.application.name}`);
-        cachedApps[appId] = socket.application;
-        return socket.application;
-    } catch {
-        console.log(`Lookup failed for ${appId}`);
-        return null;
-    }
+interface SettingsProps {
+    appIds: AppIdSetting[];
+    update: () => void;
+    save: () => void;
 }
 
 function isValidSnowflake(v: string) {
@@ -37,20 +23,30 @@ function isValidSnowflake(v: string) {
 }
 
 export function ReplaceTutorial() {
+    const activities: Activity[] = PresenceStore.getActivities(UserStore.getCurrentUser().id);
     return (
         <>
-            <Forms.FormTitle tag="h3">How to get an Application ID</Forms.FormTitle>
+            <Forms.FormTitle tag="h3">IDs of currently running activities</Forms.FormTitle>
+            {
+                activities.length === 0 ? <Forms.FormText>No running activities</Forms.FormText> : activities.map(activity => { return activity.flags !== 48 ? <Forms.FormText>{activity.name}: {activity.application_id}</Forms.FormText> : null; /* hide spotify */ })
+            }
+            <Forms.FormTitle tag="h3" className={Margins.top8}>Available variables</Forms.FormTitle>
             <Forms.FormText>
-                The method of getting an app's id will differ depending on what app it is. If the source code is available you can most likely find it inside the app's repository.
+                In all fields (except stream URL), you can put in variables that'll automatically be replaced by their original content:
+                <pre style={{ fontFamily: "monospace" }}>
+                    :name:, :details:, :state:
+                    <br />
+                    :large_image:, :large_text:, :small_image:, :small_text:
+                </pre>
             </Forms.FormText>
+            <Forms.FormTitle tag="h3" className={Margins.top8}>More details</Forms.FormTitle>
             <Forms.FormText>
-                Another method is to start the app in question, then open Discord's console and look for a log from RPCServer saying something like
-                <code>"cmd: 'SET_ACTIVITY'"</code> with your app's name somewhere inside
+                Leave a field empty to leave it as is.
+                <br />
+                Set a field to "null" to hide it on the presence.
+                <br />
+                You may need to reload Discord for changes to apply.
             </Forms.FormText>
-
-            <Forms.FormTitle tag="h3" style={{ color: "var(--text-danger)", textAlign: "center" }}>
-                Note: ActivityTypes other than Playing will only show timestamps on Mobile. It's a Discord issue.
-            </Forms.FormTitle>
         </>
     );
 }
@@ -62,14 +58,6 @@ export function ReplaceSettings({ appIds, update, save }: SettingsProps) {
 
         appIds[index][key] = val;
 
-        if (val && key === "appId") {
-            const tempApp = await lookupApp(val.toString());
-            appIds[index].appName = tempApp?.name || "Unknown";
-        }
-
-        if (appIds[index].appId === "" && index !== appIds.length - 1)
-            appIds.splice(index, 1);
-
         save();
         update();
     }
@@ -78,64 +66,156 @@ export function ReplaceSettings({ appIds, update, save }: SettingsProps) {
         <>
             {
                 appIds.map((setting, i) =>
-                    <Card style={{ padding: "1em 1em 0" }}>
-                        <Switch
-                            value={setting.enabled}
-                            onChange={value => {
-                                onChange(value, i, "enabled");
-                            }}
-                            className={Margins.bottom16}
-                            hideBorder={true}
-                        >
-                            {setting.appName}
-                        </Switch>
-                        <Forms.FormTitle>Application ID</Forms.FormTitle>
+                    <Card style={{ padding: "1em", opacity: !setting.enabled ? "60%" : "" }} key={i}>
+                        {
+                            isValidSnowflake(setting.appId) ?
+                                <Switch
+                                    value={setting.enabled}
+                                    onChange={value => {
+                                        onChange(value, i, "enabled");
+                                    }}
+                                    className={Margins.bottom8}
+                                    hideBorder={true}
+                                >
+                                    Apply edits to app
+                                </Switch> : <Forms.FormTitle tag="h3">Add new application</Forms.FormTitle>
+                        }
+                        <Forms.FormTitle className={`${Margins.top8} ${Margins.bottom8}`}>Application ID</Forms.FormTitle>
                         <CheckedTextInput
                             value={setting.appId}
                             onChange={async v => {
                                 onChange(v, i, "appId");
                             }}
                             validate={v =>
-                                !v || isValidSnowflake(v) || "Invalid appId, must be a snowflake"
+                                !v || isValidSnowflake(v) || "Invalid application ID"
                             }
                         />
-                        {setting.activityType === ActivityType.STREAMING &&
-                            <>
-                                <Forms.FormTitle>Stream URL</Forms.FormTitle>
-                                <CheckedTextInput
-                                    value={setting.streamUrl}
-                                    onChange={async v => {
-                                        onChange(v, i, "streamUrl");
+                        {
+                            isValidSnowflake(setting.appId) && <>
+                                <Forms.FormTitle className={Margins.top8}>New activity type</Forms.FormTitle>
+                                <Select
+                                    options={[
+                                        { label: "Playing", value: ActivityType.PLAYING },
+                                        { label: "Watching", value: ActivityType.WATCHING },
+                                        { label: "Listening", value: ActivityType.LISTENING },
+                                        { label: "Competing", value: ActivityType.COMPETING },
+                                        { label: "Streaming", value: ActivityType.STREAMING }
+                                    ]}
+                                    select={value => {
+                                        onChange(value, i, "newActivityType");
                                     }}
-                                    validate={st => !/https?:\/\/(www\.)?(twitch\.tv|youtube\.com)\/\w+/.test(st) && "Only Twitch and Youtube urls will work." || true}
+                                    className={Margins.top8}
+                                    isSelected={value => setting.newActivityType === value}
+                                    serialize={identity}
                                 />
-                            </>}
-                        <Forms.FormTitle>New activity type</Forms.FormTitle>
-                        <Select
-                            options={[
-                                { label: "Playing", value: ActivityType.PLAYING },
-                                { label: "Watching", value: ActivityType.WATCHING },
-                                { label: "Listening", value: ActivityType.LISTENING },
-                                { label: "Competing", value: ActivityType.COMPETING },
-                                { label: "Streaming", value: ActivityType.STREAMING }
-                            ]}
-                            select={value => {
-                                onChange(value, i, "activityType");
-                            }}
-                            className={Margins.bottom16}
-                            isSelected={value => setting.activityType === value}
-                            serialize={identity}
-                        />
-                        <Switch
-                            value={setting.swapNameAndDetails}
-                            onChange={value => {
-                                onChange(value, i, "swapNameAndDetails");
-                            }}
-                            className={Margins.bottom16}
-                            hideBorder={true}
-                        >
-                            Swap presence name and details
-                        </Switch>
+                                {
+                                    setting.newActivityType === ActivityType.STREAMING &&
+                                    <>
+                                        <Forms.FormTitle className={`${Margins.top8} ${Margins.bottom8}`}>Stream URL (must be YouTube or Twitch)</Forms.FormTitle>
+                                        <CheckedTextInput
+                                            value={setting.newStreamUrl}
+                                            onChange={async v => {
+                                                onChange(v, i, "newStreamUrl");
+                                            }}
+                                            validate={v => {
+                                                return /https?:\/\/(www\.)?(twitch\.tv|youtube\.com)\/\w+/.test(v) || "Invalid stream URL";
+                                            }}
+                                        />
+
+                                    </>
+                                }
+                                {
+                                    setting.newActivityType !== ActivityType.STREAMING &&
+                                    <>
+                                        <Forms.FormTitle className={Margins.top8}>New name {setting.newActivityType === ActivityType.PLAYING && "(first line)"}</Forms.FormTitle>
+                                        <TextInput
+                                            className={Margins.top8}
+                                            value={setting.newName}
+                                            onChange={async v => {
+                                                onChange(v, i, "newName");
+                                            }}
+                                        />
+                                    </>
+                                }
+                                <Forms.FormTitle className={Margins.top8}>New details {setting.newActivityType === ActivityType.PLAYING ? "(second line)" : "(first line)"}</Forms.FormTitle>
+                                <TextInput
+                                    className={Margins.top8}
+                                    value={setting.newDetails}
+                                    onChange={async v => {
+                                        onChange(v, i, "newDetails");
+                                    }}
+                                />
+                                <Forms.FormTitle className={Margins.top8}>New state {setting.newActivityType === ActivityType.PLAYING ? "(third line)" : "(second line)"}</Forms.FormTitle>
+                                <TextInput
+                                    className={Margins.top8}
+                                    value={setting.newState}
+                                    onChange={async v => {
+                                        onChange(v, i, "newState");
+                                    }}
+                                />
+                                {
+                                    !setting.disableAssets &&
+                                    <>
+                                        <Forms.FormText style={{ fontSize: "1.05rem", fontWeight: "500" }} className={Margins.top8}>Large image</Forms.FormText>
+                                        <Forms.FormTitle className={Margins.top8}>Text {setting.newActivityType !== ActivityType.PLAYING && "(also third line)"}</Forms.FormTitle>
+                                        <TextInput
+                                            className={Margins.top8}
+                                            value={setting.newLargeImageText}
+                                            onChange={async v => {
+                                                onChange(v, i, "newLargeImageText");
+                                            }}
+                                        />
+                                        <Forms.FormTitle className={Margins.top8}>URL</Forms.FormTitle>
+                                        <TextInput
+                                            className={Margins.top8}
+                                            value={setting.newLargeImageUrl}
+                                            onChange={async v => {
+                                                onChange(v, i, "newLargeImageUrl");
+                                            }}
+                                        />
+                                        <Forms.FormText style={{ fontSize: "1.05rem", fontWeight: "500" }} className={Margins.top8}>Small image</Forms.FormText>
+                                        <Forms.FormTitle className={Margins.top8}>Text</Forms.FormTitle>
+                                        <TextInput
+                                            className={Margins.top8}
+                                            value={setting.newSmallImageText}
+                                            onChange={async v => {
+                                                onChange(v, i, "newSmallImageText");
+                                            }}
+                                        />
+                                        <Forms.FormTitle className={Margins.top8}>URL</Forms.FormTitle>
+                                        <TextInput
+                                            className={Margins.top8}
+                                            value={setting.newSmallImageUrl}
+                                            onChange={async v => {
+                                                onChange(v, i, "newSmallImageUrl");
+                                            }}
+                                        />
+                                    </>
+                                }
+                                <Switch
+                                    value={setting.disableAssets}
+                                    onChange={value => {
+                                        onChange(value, i, "disableAssets");
+                                    }}
+                                    className={Margins.top8}
+                                    hideBorder={true}
+                                    style={{ marginBottom: "0" }}
+                                >
+                                    Hide assets (large & small images)
+                                </Switch>
+                                <Switch
+                                    value={setting.disableTimestamps}
+                                    onChange={value => {
+                                        onChange(value, i, "disableTimestamps");
+                                    }}
+                                    className={Margins.top8}
+                                    hideBorder={true}
+                                    style={{ marginBottom: "0" }}
+                                >
+                                    Hide timestamps
+                                </Switch>
+                            </>
+                        }
                     </Card>
                 )
             }
--- src/plusplugins/replaceActivityTypes/index.tsx
+++ src/plusplugins/replaceActivityTypes/index.tsx
@@ -1,6 +1,6 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2024 your mom lol
+ * Copyright (c) 2025 your mom lol
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -12,17 +12,72 @@ import definePlugin, { OptionType } from "@utils/types";
 import { React } from "@webpack/common";
 
 import { ReplaceSettings, ReplaceTutorial } from "./ReplaceSettings";
-import { Activity, ActivityType, AppIdSetting } from "./types";
 
 const APP_IDS_KEY = "ReplaceActivityType_appids";
+export type AppIdSetting = {
+    disableAssets: boolean;
+    disableTimestamps: boolean;
+    appId: string;
+    enabled: boolean;
+    newActivityType: ActivityType;
+    newName: string,
+    newDetails: string,
+    newState: string,
+    newLargeImageUrl: string,
+    newLargeImageText: string,
+    newSmallImageUrl: string,
+    newSmallImageText: string;
+    newStreamUrl: string;
+};
+
+export interface Activity {
+    state: string;
+    details: string;
+    timestamps?: {
+        start?: number;
+        end?: number;
+    };
+    url?: string;
+    assets: ActivityAssets;
+    buttons?: Array<string>;
+    name: string;
+    application_id: string;
+    metadata?: {
+        button_urls?: Array<string>;
+    };
+    type: number;
+    flags: number;
+}
+
+interface ActivityAssets {
+    large_image: string;
+    large_text: string;
+    small_image: string;
+    small_text: string;
+}
+
+export const enum ActivityType {
+    PLAYING = 0,
+    STREAMING = 1,
+    LISTENING = 2,
+    WATCHING = 3,
+    COMPETING = 5
+}
 
 export const makeEmptyAppId: () => AppIdSetting = () => ({
     appId: "",
-    appName: "Unknown",
-    streamUrl: "",
-    swapNameAndDetails: false,
-    activityType: ActivityType.PLAYING,
-    enabled: true
+    enabled: true,
+    newActivityType: ActivityType.PLAYING,
+    newName: "",
+    newDetails: "",
+    newState: "",
+    newLargeImageUrl: "",
+    newLargeImageText: "",
+    newSmallImageUrl: "",
+    newSmallImageText: "",
+    newStreamUrl: "",
+    disableTimestamps: false,
+    disableAssets: false
 });
 
 let appIds = [makeEmptyAppId()];
@@ -49,14 +104,13 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "ReplaceActivityTypes",
     description: "Replace the activity type (Playing) of any application's rich presence",
-    authors: [Devs.Nyako],
+    authors: [Devs.Nyako, Devs.nin0dev],
     patches: [
-        // how has this patch not broken yet lol (I do not like fixing patches tho)
         {
             find: '"LocalActivityStore"',
             replacement: {
-                match: /\i\((\i)\)\{.{0,50}activity.{0,10}=\i;/,
-                replace: "$&$self.patchActivity($1.activity);",
+                match: /\i\(\i\)\{.{0,25}activity:(\i)\}=\i;/,
+                replace: "$&$self.patchActivity($1);",
             }
         }
     ],
@@ -66,24 +120,34 @@ export default definePlugin({
     async start() {
         appIds = await DataStore.get(APP_IDS_KEY) ?? [makeEmptyAppId()];
     },
-
+    parseField(text: string, originalActivity: Activity): string {
+        if (text === "null") return "";
+        return text
+            .replaceAll(":name:", originalActivity.name)
+            .replaceAll(":details:", originalActivity.details)
+            .replaceAll(":state:", originalActivity.state)
+            .replaceAll(":large_image:", originalActivity.assets.large_image)
+            .replaceAll(":large_text:", originalActivity.assets.large_text)
+            .replaceAll(":small_image:", originalActivity.assets.small_image)
+            .replaceAll(":small_text:", originalActivity.assets.small_text);
+    },
     patchActivity(activity: Activity) {
         if (!activity) return;
-        console.log(activity);
         appIds.forEach(app => {
             if (app.enabled && app.appId === activity.application_id) {
-                activity.type = app.activityType;
-
-                if (app.activityType === ActivityType.STREAMING && app.streamUrl) {
-                    activity.url = app.streamUrl;
-                }
-
-                if (app.swapNameAndDetails) {
-                    const media = activity.details;
-                    activity.details = activity.name;
-                    activity.name = media;
-                }
-
+                const oldActivity = { ...activity };
+                activity.type = app.newActivityType;
+                if (app.newName) activity.name = this.parseField(app.newName, oldActivity);
+                if (app.newActivityType === ActivityType.STREAMING && app.newStreamUrl) activity.url = app.newStreamUrl;
+                if (app.newDetails) activity.details = this.parseField(app.newDetails, oldActivity);
+                if (app.newState) activity.state = this.parseField(app.newState, oldActivity);
+                if (app.newLargeImageText) activity.assets.large_text = this.parseField(app.newLargeImageText, oldActivity);
+                if (app.newLargeImageUrl) activity.assets.large_image = this.parseField(app.newLargeImageUrl, oldActivity);
+                if (app.newSmallImageText) activity.assets.small_text = this.parseField(app.newSmallImageText, oldActivity);
+                if (app.newSmallImageUrl) activity.assets.small_image = this.parseField(app.newSmallImageUrl, oldActivity);
+                // @ts-ignore here we are intentionally nulling assets
+                if (app.disableAssets) activity.assets = {};
+                if (app.disableTimestamps) activity.timestamps = {};
             }
         });
     },
