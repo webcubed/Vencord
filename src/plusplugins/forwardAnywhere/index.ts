910e02132ecc2403b65c2dc2f170fd4fb77bf3f2
RobinRMC <github@robinmail.nl>
2025-04-30 17:07:21 +0200

    Update ForwardAnywhere

--- src/plusplugins/forwardAnywhere/index.ts
+++ src/plusplugins/forwardAnywhere/index.ts
@@ -4,15 +4,29 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { definePluginSettings } from "@api/Settings";
 import { EquicordDevs } from "@utils/constants";
 import { sendMessage } from "@utils/discord";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 import { Message } from "discord-types/general";
+
+// Taken From Signature :)
+const settings = definePluginSettings({
+    forwardPreface: {
+        description: "What markdown to use for the \"Forwarded from\" text line",
+        type: OptionType.SELECT,
+        options: [
+            { label: "Block Quote", value: ">", default: true },
+            { label: "Subtext", value: "-#" }
+        ]
+    }
+});
 
 export default definePlugin({
     name: "ForwardAnywhere",
     description: "Sends a forwarded message as a normal message if it's not possible to forward the message. Also allows messages from channels marked as NSFW to be forwarded to channels not marked as NSFW",
     authors: [EquicordDevs.thororen],
+    settings,
     patches: [
         {
             find: "#{intl::MESSAGE_FORWARDING_NSFW_NOT_ALLOWED}",
@@ -32,7 +46,7 @@ export default definePlugin({
     sendForward(channels: any, message: Message) {
         for (const c of channels) {
             sendMessage(c.id, {
-                content: `${message.content}\n\n> Forwarded from <#${message.channel_id}>`
+                content: `${message.content}\n${settings.store.forwardPreface} Forwarded from <#${message.channel_id}>`
             });
         }
     }
