c586e79b57463f376729366ef1c1fc449d4b2048
RobinRMC <github@robinmail.nl>
2025-05-11 19:29:05 +0200

    Update plugins

--- src/plusplugins/imagePreview/index.ts
+++ src/plusplugins/imagePreview/index.ts
@@ -1,6 +1,6 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -9,14 +9,11 @@ import "./styles.css";
 import { EquicordDevs } from "@utils/constants";
 import { Logger } from "@utils/Logger";
 import definePlugin from "@utils/types";
-import { findStoreLazy } from "@webpack";
+import { StickersStore } from "@webpack/common";
 
 import { getMimeType, isLinkAnImage, settings, stripDiscordParams } from "./settings";
 
 const logger = new Logger("ImagePreview", "#FFFFFF");
-const StickerStore = findStoreLazy("StickersStore") as {
-    getStickerById(id: string): any;
-};
 
 let currentPreview: HTMLDivElement | null = null;
 let currentPreviewFile: HTMLImageElement | HTMLVideoElement | null = null;
@@ -124,7 +121,7 @@ function loadImagePreview(url: string, sticker: boolean) {
 
     if (sticker) {
         const stickerId = url.split("/").pop()?.split(".")[0] ?? null;
-        const stickerData = stickerId ? StickerStore.getStickerById(stickerId) : null;
+        const stickerData = stickerId ? StickersStore.getStickerById(stickerId) : null;
 
         if (stickerData) {
             switch (stickerData.type) {
@@ -506,7 +503,7 @@ export default definePlugin({
             observer = createObserver();
             observer.observe(targetNode, { childList: true, subtree: true });
         } else {
-            logger.info("body wasnt found ?????????");
+            logger.info("Body couldn't be found.");
         }
     },
 
